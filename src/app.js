755e37504353b142c72e21ee074b3aff990a7bbd
BrunoF2P <brunobispof2p@gmail.com>
2024-09-12 00:31:49 -0300

    Adicionando documentador

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import cors from 'cors'
-import dotenv from 'dotenv'
+import cors from 'cors';
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import routers from './routes/index.js';
+import YAML from 'yamljs';
+import swaggerUi from 'swagger-ui-express';
+import passport from "./bin/passport.js";
 
 // Configurando variaveis de ambiente
 dotenv.config();
@@ -11,12 +14,19 @@ dotenv.config();
 // Criando uma instância do aplicativo Express
 const app = express();
 
+// Carrega o arquivo YAML para Swagger
+const swaggerDocument = YAML.load('docs/swagger.yaml');
+
 // Configurando middlewares
 app.use(logger('dev'));
 app.use(cors());
+app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Documentação do Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Configurando rotas
 app.use('/v1', routers);

ca7fd0065258fb237f33a2c11d3996f413a13c92
BrunoF2P <brunobispof2p@gmail.com>
2024-09-12 00:36:16 -0300

    adicionando login e validadores de campo

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,19 @@
 import { Router } from 'express';
-import {registerUser} from '../controller/userController.js';
-import {validationRules, validateUser, tipoUsuarioValidation} from '../middleware/validationUserMiddleware.js';
+import {registerUser, loginUser} from '../controller/userController.js';
+import {validadeLogin, validationRegister, validateTipeUser} from '../middleware/validationUserMiddleware.js';
 
 const router = Router();
 
 router.post('/register',
-    validationRules,
-    validateUser,
-    tipoUsuarioValidation,
+    validationRegister,
+    validateTipeUser,
     registerUser
+);
+
+router.post('/login',
+    validadeLogin,
+    loginUser
+
 );
 
 export default router;

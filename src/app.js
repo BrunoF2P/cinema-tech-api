abb56649667b7183fb548a32fb42ec9c930a836c
BrunoF2P <brunobispof2p@gmail.com>
2024-09-14 23:30:09 -0300

    configurando swagger para documentacao em producao e local

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const app = express();
 
 // Carrega o arquivo YAML para Swagger
 const swaggerDocument = YAML.load('docs/swagger.yaml');
+swaggerDocument.servers[1].url = process.env.SERVER_URL
 
 // Configurando middlewares
 app.use(logger('dev'));
@@ -26,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Documentação do Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Configurando rotas
 app.use('/v1', routers);

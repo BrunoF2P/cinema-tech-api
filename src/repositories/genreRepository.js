e1b73be6888a16209bf9ab048de97b2cfbbe8f44
BrunoF2P <brunobispof2p@gmail.com>
2024-09-27 23:54:32 -0300

    usando generic no genreRepository

--- src/repositories/genreRepository.js
+++ src/repositories/genreRepository.js
@@ -1,39 +1,27 @@
-import prisma from "../../prismaClient.js";
+import { findAll, findByUnique, create, findByField, update, deleteById } from './genericRepository.js';
 
 async function getAllGenres() {
-    return prisma.genero.findMany();
+    return findAll('genero');
 }
 
 async function getGenreById(id) {
-    return prisma.genero.findUnique({
-        where: { id_genero: id },
-    });
+    return findByUnique('genero', 'id_genero', id);
 }
 
 async function createGenre(genreData) {
-    return prisma.genero.create({
-        data: genreData,
-    });
+    return create('genero', genreData);
 }
 
 async function getGenreByName(name) {
-    return prisma.genero.findUnique({
-        where: { nome_genero: name },
-    });
+    return findByUnique('genero', 'nome_genero', name);
 }
 
-
 async function updateGenre(id, genreData) {
-    return prisma.genero.update({
-        where: { id_genero: id },
-        data: genreData,
-    });
+    return update('genero', 'id_genero', id, genreData);
 }
 
 async function deleteGenre(id) {
-    return prisma.genero.delete({
-        where: { id_genero: id },
-    });
+    return deleteById('genero', 'id_genero', id);
 }
 
-export { getAllGenres, getGenreById, createGenre, updateGenre, deleteGenre, getGenreByName};
+export { getAllGenres, getGenreById, createGenre, updateGenre, deleteGenre, getGenreByName };

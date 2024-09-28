6244b715df315f10d9e6e28173c94677e5d409f9
BrunoF2P <brunobispof2p@gmail.com>
2024-09-28 00:54:22 -0300

    Adicionando tipos de sala e salas as rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,8 @@ import filmRoutes from "./filmRoutes.js";
 import genreRoutes from "./genreRoutes.js";
 import stateRoutes from "./stateRoutes.js";
 import cityRoutes from "./cityRoutes.js";
+import typeRoomRoutes from "./typeRoomRoutes.js";
+import roomRoutes from "./roomRoutes.js";
 
 const router = Router();
 
@@ -12,7 +14,8 @@ router.use('/', userRoutes);
 router.use('/', stateRoutes)
 router.use('/', filmRoutes);
 router.use('/', genreRoutes);
-router.use('/', cityRoutes)
-
+router.use('/', cityRoutes);
+router.use('/', typeRoomRoutes);
+router.use('/', roomRoutes);
 
 export default router;

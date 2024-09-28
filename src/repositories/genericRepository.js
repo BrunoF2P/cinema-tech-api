de961b12405de87aee60daab71d948f25740511e
BrunoF2P <brunobispof2p@gmail.com>
2024-09-28 00:51:52 -0300

    fixup! criando generic para repository

---
+++ src/repositories/genericRepository.js
@@ -0,0 +1,52 @@
+import prisma from "../../prismaClient.js";
+
+async function findAll(model, include = null) {
+    return prisma[model].findMany({
+        ...(include && { include }),
+    });
+}
+
+async function findByUnique(model, uniqueField, id, include = null) {
+    const where = {};
+    where[uniqueField] = id;
+    return prisma[model].findUnique({
+        where,
+        ...(include && { include }),
+    });
+}
+
+async function create(model, data, include = null) {
+    return prisma[model].create({
+        data,
+        ...(include && { include }),
+    });
+}
+
+async function update(model, idField, id, data, include = null) {
+    const where = {};
+    where[idField] = id;
+    return prisma[model].update({
+        where,
+        data,
+        ...(include && { include }),
+    });
+}
+
+async function deleteById(model, idField, id) {
+    const where = {};
+    where[idField] = id;
+    return prisma[model].delete({
+        where,
+    });
+}
+
+async function findByField(model, fieldName, fieldValue, include = null) {
+    const where = {};
+    where[fieldName] = fieldValue;
+    return prisma[model].findMany({
+        equals: where,
+        ...(include && { include }),
+    });
+}
+
+export { findAll, findByUnique, create, update, deleteById, findByField };

5170719e37f2c1560b2f8ca72ded08e3630f40ab
BrunoF2P <brunobispof2p@gmail.com>
2024-10-03 02:26:44 -0300

    refactor(session): melhora a resposta do json

--- src/repositories/sessionRepository.js
+++ src/repositories/sessionRepository.js
@@ -2,17 +2,74 @@ import {findAll, findByUnique, create, update, deleteById, findByField, checkFie
 
 async function getAllSession(){
     return await findAll('sessao', {
-        filme: true,
-        sala: true,
+        filme: {
+            select: {
+                id_filme: true,
+                titulo: true,
+                sinopse: true,
+                data_lancamento: true,
+                duracao: true,
+                classificacao_etaria: true,
+                poster_path: true,
+                nota_imdb: true
+            }
+        },
+        sala: {
+            select: {
+                id_tipo_sala: true,
+                nome_sala: true,
+                capacidade: true,
+                ativo: true
+            }
+        },
     });
 }
 
-async function getSessionById(id){
-    return await findByUnique('sessao', 'id_sessao', id,{
-        filme: true,
-        sala: true,
-    });
+async function getSessionById(id) {
+    return await findByUnique('sessao', 'id_sessao', id, {
+
+            filme: {
+                select: {
+                    id_filme: true,
+                    titulo: true,
+                    sinopse: true,
+                    data_lancamento: true,
+                    duracao: true,
+                    classificacao_etaria: true,
+                    poster_path: true,
+                    nota_imdb: true
+                }
+            },
+            sala: {
+                select: {
+                    id_sala: true,
+                    nome_sala: true,
+                    capacidade: true,
+                    ativo: true,
+                    id_tipo_sala: true,
+                    tipoSala: {
+                        select: {
+                            descricao: true
+                        }
+                    }
+                }
+            },
+            PrecoIngresso: {
+                select: {
+                    id_preco: true,
+                    preco: true,
+                    id_tipo: true,
+                    tipo: {
+                        select: {
+                            descricao: true
+                        }
+                    }
+                }
+            }
+        }
+    );
 }
+
 
 async function getSessionByMovieId(id) {
     return await findByField('sessao', 'id_filme', id,{

d5615a3a752d77bbe39481f42469495200837b33
BrunoF2P <brunobispof2p@gmail.com>
2024-10-04 03:24:34 -0300

    refactor: mudando ordem de chama do cookieParser

--- src/app.js
+++ src/app.js
@@ -25,10 +25,10 @@ swaggerDocument.servers[0].url = `${process.env.SERVER_URL}/v1`;
 // Configurando middlewares
 app.use(logger('dev'));
 app.use(cors());
+app.use(cookieParser());
 app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public'));

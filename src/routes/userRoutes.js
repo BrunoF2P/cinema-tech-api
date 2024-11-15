3d96c5ee58414e3f7cde25f28c8b908a2b41cbf2
BrunoF2P <brunobispof2p@gmail.com>
2024-11-14 22:30:07 -0300

    feat: rota pra verificar se usuario é administrador ou usuario comum

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import {registerUser, loginUser} from '../controllers/userController.js';
 import {validadeLogin, validationRegister, validateTipeUser} from '../middlewares/userMiddleware.js';
+import {authenticateJwt, authorizeAdmin} from "../middlewares/authMiddleware.js";
 
 const router = Router();
+
+
+router.get('/verify', authenticateJwt, authorizeAdmin, (req, res) => {
+    res.status(200).json({ success: true, msg: 'Bem-vindo ao painel de administração!' });
+});
 
 router.post('/register',
     validationRegister,

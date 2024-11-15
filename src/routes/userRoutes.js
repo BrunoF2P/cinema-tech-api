c6f888d2c7a02482995009016366341315ac5a4d
BrunoF2P <brunobispof2p@gmail.com>
2024-11-14 22:44:03 -0300

    feat: Definicação explicita se é admin, usuario comum ou n autorizado

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,8 +6,31 @@ import {authenticateJwt, authorizeAdmin} from "../middlewares/authMiddleware.js"
 const router = Router();
 
 
-router.get('/verify', authenticateJwt, authorizeAdmin, (req, res) => {
-    res.status(200).json({ success: true, msg: 'Bem-vindo ao painel de administração!' });
+router.get('/verify', authenticateJwt, async (req, res) => {
+    try {
+        // Verificando se o usuário é um admin
+        if (req.user && req.user.id_tipo_usuario === 1) {
+            return res.status(200).json({
+                success: true,
+                msg: 'Usuário autenticado como admin',
+                role: 'admin',
+            });
+        }
+
+        // Se não for admin, retornamos um status 200, mas com a role "user"
+        return res.status(200).json({
+            success: true,
+            msg: 'Usuário autenticado como comum',
+            role: 'user',
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            success: false,
+            msg: 'Erro interno no servidor',
+            error: err,
+        });
+    }
 });
 
 router.post('/register',
